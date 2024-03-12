708f513cee6b9414ad73b621f790b9fd1618e166
shaydeecoder <shaydeecoder@gmail.com>
2024-03-12 13:07:42 +0100

    refactor: Configure webpack for TypeScript

--- dist/drag-drop-interfaces.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=drag-drop-interfaces.js.map
---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+    compress: true,
+    port: 3001,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html", // Path to your HTML file
+    }),
+  ],
+};
